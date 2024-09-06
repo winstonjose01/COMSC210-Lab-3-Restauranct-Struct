4b6d770890e03e3035ee7a9773f6e044c38aa66a
winstonjose01 <winston.jose@gmail.com>
2024-09-06 11:35:09 -0700

    revision-4

--- restaurant_struct.cpp
+++ restaurant_struct.cpp
@@ -1,10 +1,11 @@
 # include <iostream>
 # include <limits>
+# include <iomanip>
 using namespace std;
 
 struct Restaurant {
     string name;
-    string dining_type;
+    string dining_type; //Fast-food, Casual, Coffee-Shop, 
     string cuisine_type; // Mexican, Chinese, etc.
     string pricing_type; // Budget, Mid-range, Fine-dining
     int review;
@@ -13,10 +14,19 @@ struct Restaurant {
 
 Restaurant populateRestaurant();
 void outputRestaurant(const Restaurant &);
+void printHeader();
 
 int main() {
-    //Create a single Restaurant object and populate
-    Restaurant myRestaurant = populateRestaurant();
+    //Create 4 Restaurant object and populate
+    Restaurant myRestaurant1 = populateRestaurant();
+    //Restaurant myRestaurant2 = populateRestaurant();
+    //Restaurant myRestaurant3 = populateRestaurant();
+    //Restaurant myRestaurant4 = populateRestaurant();
+    printHeader();
+    outputRestaurant(myRestaurant1);
+    //outputRestaurant(myRestaurant2);
+    //outputRestaurant(myRestaurant3);
+    //outputRestaurant(myRestaurant4);
 
 return 0;
 }
@@ -30,7 +40,7 @@ Restaurant populateRestaurant() {
      getline(cin, temp.dining_type);
      cout << "Enter the type of cuisine: ";
      getline(cin, temp.cuisine_type);
-     cout << "Enter the pricing value for this restaurant ";
+     cout << "Enter the pricing value for this restaurant: ";
      getline(cin, temp.pricing_type);
      cout << "Enter the review rating (1 - 5) for this restaurant: ";
      while (true) {
@@ -46,16 +56,34 @@ Restaurant populateRestaurant() {
             break;
             }
      }
-
-
+    return temp;
 }
 
+void printHeader (){
+    cout << left 
+        << setw(20) << "Name" 
+        << setw(22) << "Dining Type" 
+        << setw(20) << "Cuisine" 
+        << setw(20) << "Price/Value" 
+        << setw(20) <<"Rating" 
+        << endl;
+    cout << left 
+        << setw(20) << "----" 
+        << setw(22) << "-----------" 
+        << setw(20) << "--------" 
+        << setw(20) << "----------" 
+        << setw(20) << "------" 
+        << endl;
+}
+
+
 void outputRestaurant (const Restaurant &rest){
-    cout << "\t Name: " << rest.name;
-    cout << "\t Type of Dining: " << rest.dining_type;
-    cout << "\t Type of Cuisine: " << rest.dining_type;
-    cout << "\t Type of Pricing/Value :" << rest.pricing_type;
-    cout << "\t Review rating :" << rest.review;
-    cout << '\n';
+    cout << left 
+        << setw(20) << rest.name 
+        << setw(22) << rest.dining_type 
+        << setw(20) << rest.cuisine_type 
+        << setw(20) << rest.pricing_type 
+        << setw(20)<< rest.review 
+        << endl;
 
 }
