85f4f8a76312557a9132866ef27647f4c13ae141
winstonjose01 <winston.jose@gmail.com>
2024-09-06 10:35:41 -0700

    re

--- restaurant_struct.cpp
+++ restaurant_struct.cpp
@@ -17,6 +17,26 @@ int main() {
     //Create a single Restaurant object and populate
     Restaurant myRestaurant = populateRestaurant();
 
-
 return 0;
+}
+
+Restaurant populateRestaurant() {
+    Restaurant temp;
+     cout << "Enter the name of the restaurant: ";
+     getline(cin,temp.name);
+     cin.ignore();
+     cout << "Enter the type of dining establishment: ";
+     getline(cin, temp.dining_type);
+     cout << "Enter the type of cuisine: ";
+     getline(cin, temp.cuisine_type);
+     cout << "Enter the pricing value for this restaurant ";
+     getline(cin, temp.pricing_type);
+     cout << "Enter the review rating (1 - 5) for this restaurant: ";
+     cin >> temp.review;
+     cin.ignore();
+
+}
+
+void outputRestaurant (const Restaurant &rest){
+    
 }
